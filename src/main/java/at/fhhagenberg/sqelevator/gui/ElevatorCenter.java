ba7f066ca2b92c7fe3ebb110d9046247119ee50e
dicley <engdicleyilmaz@gmail.com>
2019-12-25 00:26:58 +0300

    sonar cube test

--- src/main/java/at/fhhagenberg/sqelevator/gui/ElevatorCenter.java
+++ src/main/java/at/fhhagenberg/sqelevator/gui/ElevatorCenter.java
@@ -1,24 +1,11 @@
 package at.fhhagenberg.sqelevator.gui;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ElevatorCenter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        var root = new VBox();
-        root.setSpacing(20);
-        root.setPadding(new Insets(10,10,10,10));
-
-        root.getChildren().add(new Label("Test"));
-
-        var scene = new Scene(root, 500, 200);
-        stage.setScene(scene);
-        stage.setTitle("Hello World");
-        stage.show();
+        
     }
 }
